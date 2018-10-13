24f02ac084a27e54735e1b88357c919d558910e8
Berat Çiçek <berat@vngrs.com>
2018-10-13 22:32:12 +0300

    Write unit test for obfuscation class

--- BCObfuscationExample/BCObfuscationExampleTests/BCObfuscationExampleTests.swift
+++ BCObfuscationExample/BCObfuscationExampleTests/BCObfuscationExampleTests.swift
@@ -10,27 +10,41 @@ import XCTest
 @testable import BCObfuscationExample
 
 class BCObfuscationExampleTests: XCTestCase {
+
+    let obfuscation = BCObfuscation()
+    let obfuscationWithKey = BCObfuscation(with: "Hallo!")
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testEncrptHelloText() {
+
+        let encrypted: [UInt8] = [6, 54, 35, 14, 5]
+
+        XCTAssertEqual(obfuscation.obfuscate(with: "Hello"), encrypted)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+
+    func testDecryptHelloText() {
+
+        let encrypted: [UInt8] = [6, 54, 35, 14, 5]
+
+        XCTAssertEqual(obfuscation.resolve(with: encrypted), "Hello")
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    func testCompareEncryptionWithDifferentKeys() {
+
+        let plainText = "Hello"
+
+        XCTAssertNotEqual(
+            obfuscation.obfuscate(with: plainText),
+            obfuscationWithKey.obfuscate(with: plainText)
+        )
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+
+    func testCompareDecryptionWithDifferentKeys() {
+
+        let encrypted: [UInt8] = [6, 54, 35, 14, 5]
+
+        XCTAssertNotEqual(
+            obfuscation.resolve(with: encrypted),
+            obfuscationWithKey.resolve(with: encrypted)
+        )
     }
-    
 }
