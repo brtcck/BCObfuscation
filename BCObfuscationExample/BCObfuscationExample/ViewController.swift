ace181c77628a7b6f2da532fbaf3578a98613d62
Berat Çiçek <berat@vngrs.com>
2018-10-13 21:34:28 +0300

    Initial commit

---
+++ BCObfuscationExample/BCObfuscationExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BCObfuscationExample
+//
+//  Created by Berat Çiçek on 13.10.2018.
+//  Copyright © 2018 Berat Çiçek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
